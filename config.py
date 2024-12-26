a7d774ca48183f612c91d6cf3df87c3b7836d0db
mukha <mukhammedismailov96@gmail.com>
2024-12-26 17:15:55 +0300

    main

---
+++ config.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.resnet import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
